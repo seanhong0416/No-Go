40505d3a14b65b4626d94a59833879bf35864c14
rrreol <seanhong0416@gapp.nthu.edu.tw>
2022-12-17 12:48:42 +0800

    RAVE done, not yet checked

---
+++ MC_RAVE.cpp
@@ -0,0 +1,172 @@
+/*
+Agent with Monte Carlo Tree Search
+*/
+
+#pragma once
+#include <string>
+#include <random>
+#include <sstream>
+#include <map>
+#include <type_traits>
+#include <algorithm>
+#include <fstream>
+#include <cmath>
+#include "board.h"
+#include "action.h"
+#include "agent.h"
+#include "MC_RAVE.h"
+
+Node * MC_RAVE::selection(Node * parent){
+    std::vector<float> v(parent->kids.size());
+    float move_mean;
+    float RAVE_mean;
+    float MC_RAVE_weight;
+    //get the value for each child node
+    for(int i=0;i<parent->kids.size();i++){
+        //if child has not been traversed before
+        if(parent->kids[i]->nb == 0){
+            return parent->kids[i];
+        }
+        else{
+            move_mean = (float)(-parent->kids[i]->value)/parent->kids[i]->nb;
+            RAVE_mean = (float)(-parent->kids[i]->RAVE_value)/parent->kids[i]->RAVE_nb;
+            MC_RAVE_weight = sqrt((float)(equivalence_parameter)/(3*parent->nb+equivalence_parameter));
+            v[i] = (1-MC_RAVE_weight)*move_mean + MC_RAVE_weight*RAVE_mean + sqrt(2*log(parent->nb)/parent->kids[i]->nb);
+        }
+    }
+    //get the index of the maximum 
+    int max_index = -1;
+    float max_value = -std::numeric_limits<float>::infinity();
+    for(int i=0;i<v.size();i++){
+        //printf("index : %d, v_vector : %f, value : %d, nb : %d, parent nb : %d\n",i,v[i], parent->kids[i]->value, parent->kids[i]->nb, parent->nb);
+        if(v[i] > max_value){
+            max_index = i;
+            max_value = v[i];
+        }    
+    }
+    if(max_index == -1) printf("error when selecting max child\n");
+
+    //printf("return index : %d\n",max_index);
+    return parent->kids[max_index];
+}
+
+int MC_RAVE::random_simulation(Node * start, board::piece_type who_start){
+    board current_board = start->b;
+    board trial = start->b;
+    board::piece_type who_current = who_start;
+    std::vector<action::place> space_white(board::size_x * board::size_y);
+    std::vector<action::place> space_black(board::size_x * board::size_y);
+    for (size_t i = 0; i < space.size(); i++){
+        space_white[i] = action::place(i, board::piece_type::white);
+        space_black[i] = action::place(i, board::piece_type::black);
+    }
+    std::vector<board> possible_boards;
+
+    while(1){
+        //initialize
+        //find all possible outcome boards
+        if(who_current == board::piece_type::white){
+            for (const action::place& move : space_white) {
+                board trial = current_board;
+                if (move.apply(trial) == board::legal)
+                    possible_boards.push_back(trial);
+            }
+        }
+        else{
+            for (const action::place& move : space_black) {
+                board trial = current_board;
+                if (move.apply(trial) == board::legal)
+                    possible_boards.push_back(trial);
+            }
+        }
+
+        //check if it is the end of the game
+        if(possible_boards.size() == 0){
+            if(who_current == who_start){
+                return -1;
+            }
+            else return 1;
+        }
+        else{
+            //randomly pick one action to proceed
+            std::shuffle(possible_boards.begin(), possible_boards.end(), engine);
+            current_board = possible_boards[0];
+            trial = current_board;
+            possible_boards.clear();
+            if(who_current == board::piece_type::white) who_current = board::piece_type::black;
+            else who_current = board::piece_type::white;
+        }
+    }
+}
+
+void MC_RAVE::update_value(std::vector<Node *>& route, int v){
+    int route_len = route.size();
+    std::vector<action::place> RAVE_action;
+    if(route_len != 0)
+        RAVE_action.push_back(route[route_len-1]->action_taken);
+    int RAVE_action_len = RAVE_action.size();
+
+    for(int i=route_len-2;i>=0;i--){
+        //update move/RAVE value
+        route[i]->value += v;
+        route[i]->nb += 1;
+        route[i]->RAVE_value += v;
+        route[i]->RAVE_nb += 1;
+        //update RAVE value
+        for(int j=RAVE_action_len-3;j>=0;j-=2){
+            //see if any children of this node took the same actions
+            int kids_len = route[i]->kids.size();
+            for(int k=0;k<kids_len;k++){
+                if(route[i]->kids[k]->action_taken == RAVE_action[j]){
+                    //-v because it is the value of next state
+                    route[i]->kids[k]->RAVE_value -= v;
+                    route[i]->kids[k]->RAVE_nb += 1;
+                }
+            }
+        }
+        //opponent's turn
+        v = -v;
+        //update RAVE_action_len
+        RAVE_action.push_back(route[i]->action_taken);
+        RAVE_action_len = RAVE_action.size();
+    }
+}
+
+void Node::new_kids(){
+    //need to know the color of player of next state
+    board::piece_type next_who;
+
+    //expand all kids by all legal moves
+    if(who == board::piece_type::white){
+        next_who = board::piece_type::black;
+        std::vector<action::place> space_white(board::size_x * board::size_y);
+        for (size_t i = 0; i < space_white.size(); i++){
+            space_white[i] = action::place(i, board::piece_type::white);
+        }
+        //printf("enter check illegal move white\n");
+        for (const action::place& move : space_white) {
+            board after = b;
+            if (move.apply(after) == board::legal)
+                kids.push_back(new Node(after, next_who, move));
+        }
+    }
+    else{
+        next_who = board::piece_type::white;
+        std::vector<action::place> space_black(board::size_x * board::size_y);
+        for (size_t i = 0; i < space_black.size(); i++){
+            space_black[i] = action::place(i, board::piece_type::black);
+        }
+        //printf("enter check illegal move black\n");
+        for (const action::place& move : space_black) {
+            board after = b;
+            if (move.apply(after) == board::legal)
+                kids.push_back(new Node(after, next_who, move));
+        }
+
+    }
+    
+    //printf("size of kids : %d\n",kids.size());
+
+    return;
+}
+
---
+++ MC_RAVE.h
@@ -0,0 +1,175 @@
+/*
+Agent with Monte Carlo Tree Search
+*/
+
+#pragma once
+#include <string>
+#include <random>
+#include <sstream>
+#include <map>
+#include <type_traits>
+#include <algorithm>
+#include <fstream>
+#include <limits>
+#include "board.h"
+#include "action.h"
+#include "agent.h"
+
+class Node{
+public:
+    Node(board b_in, board::piece_type who_in, action::place action_in)
+    {
+		who = who_in;
+		b = b_in;
+		action_taken = action_in;
+		value = 0;
+		nb = 0;
+		RAVE_value = 0;
+		RAVE_nb = 0;
+	}
+
+	void new_kids();
+
+	std::vector<Node *> kids;      
+    board::piece_type who;                  
+
+	board b;
+	action::place action_taken;
+    int value;
+	int nb;
+	int RAVE_value;
+	int RAVE_nb;                      
+};
+
+class MC_RAVE : public random_agent{
+public:
+	MC_RAVE(const std::string& args = "") : random_agent("name=random role=unknown " + args),
+		space(board::size_x * board::size_y), who(board::empty) {
+		printf("MCTS player initialized\n");
+		iteration_num = 1000;
+		equivalence_parameter = 500;
+		if (meta.find("T") != meta.end())
+			iteration_num = int(meta["T"]);
+		if (meta.find("k") != meta.end())
+			equivalence_parameter = int(meta["k"]);
+		if (name().find_first_of("[]():; ") != std::string::npos)
+			throw std::invalid_argument("invalid name: " + name());
+		if (role() == "black") who = board::black;
+		if (role() == "white") who = board::white;
+		if (who == board::empty)
+			throw std::invalid_argument("invalid role: " + role());
+		for (size_t i = 0; i < space.size(); i++)
+			space[i] = action::place(i, who);
+	}
+
+	Node * selection(Node * parent);
+
+	int random_simulation(Node * start, board::piece_type who_start);
+
+	void update_value(std::vector<Node *>& route, int v);
+
+	void playOneSequence(Node* root){
+		int i = 0;
+		int simulation_value;
+		std::vector<Node *> route;
+		route.clear();
+		route.push_back(root);
+		while(1){
+			//printf("route : %d\n",i);
+			//check if route[i] is a leaf
+			if(route[i]->kids.size() == 0){
+				//if it is a leaf that hasn't been traverse,
+				//expand its kids and then do simulation
+				if(route[i]->nb == 0){
+					//printf("it is a leaf that hasn't been traversed\n");
+					route[i]->new_kids();
+					std::shuffle(route[i]->kids.begin(), route[i]->kids.end(), engine);
+					//printf("number of kids after new_kids : %d\n", (int)route[i]->kids.size());
+					//simulate
+					simulation_value = random_simulation(route[i], route[i]->who);
+					//update value and nb of the last node
+					route[i]->nb += 1;
+					route[i]->value = simulation_value;
+					route[i]->RAVE_nb += 1;
+					route[i]->RAVE_value += simulation_value;
+					//printf("sim_val = %d, nb = %d, value = %d\n", simulation_value, route[i]->nb, route[i]->value);
+				}
+				//else it is the end of the game
+				else{
+					//printf("it is a leaf that is the end of game\n");
+					//simulate
+					simulation_value = random_simulation(route[i], route[i]->who);	
+					//update value and nb of the last node
+					route[i]->nb += 1;
+					route[i]->value += simulation_value;
+					route[i]->RAVE_nb += 1;
+					route[i]->RAVE_value += simulation_value;
+				}
+				//update the value of each node in the route
+				update_value(route, -simulation_value);
+				break;
+			}
+			//not a leaf, continue selection
+			//printf("select next kid\n");
+			Node * next_kid = selection(route[i]);
+			route.push_back(next_kid);
+			i++;
+		}
+	}
+
+	void print_tree(Node * root, Node * parent){
+		int kids_len = root->kids.size();
+		printf("value : %d, nb : %d, parent value : %d, parent nb : %d\n",root->value,root->nb,parent->value,parent->nb);
+		for(int i=0;i<kids_len;i++){
+			print_tree(root->kids[i], root);
+		}
+	}
+
+	void delete_tree(Node * root){
+		int kids_len = root->kids.size();
+		for(int i=0;i<kids_len;i++){
+			delete_tree(root->kids[i]);
+		}
+		delete(root);
+	}
+
+	virtual action take_action(const board& state) {
+		//initialize
+		//action in rootNode doesn't mean anything
+		Node * rootNode = new Node(state, who, action());
+		//do MCTS
+		for(int i=0;i<iteration_num;i++){
+			//printf("iteration : %d\n", i);
+			//printf("playOneSequence\n");
+			playOneSequence(rootNode);
+		}
+		//print_tree(rootNode, rootNode);
+		//std::cout << state << std::endl;
+		action::place best_action;
+		int best_visit_count = -1;
+		int rootNode_kids_len = (int)rootNode->kids.size();
+		if(rootNode_kids_len != 0){
+			for(int i=0;i<rootNode_kids_len;i++){
+				//printf("kid %d visited %d\n",i,rootNode->kids[i]->nb);
+				if(rootNode->kids[i]->nb > best_visit_count){
+					best_visit_count = rootNode->kids[i]->nb;
+					best_action = rootNode->kids[i]->action_taken;
+				}
+			}
+			//std::cout << "best action chosen = " << best_action << std::endl;
+			delete_tree(rootNode);
+			return best_action;
+		}
+		else{
+			std::cout << "no moves left" << std::endl;
+			delete_tree(rootNode);
+			return action();
+		}
+	}
+
+private:
+	std::vector<action::place> space;
+	board::piece_type who;
+	int iteration_num;
+	int equivalence_parameter;
+};
